6484f38db997b165f9ec713dea0b634c7c2e4591
Peter Garside-Quin <petegq@gmail.com>
2022-05-24 13:13:19 +0100

    Update CreateChoice zod to text

--- app/choices/mutations/createChoice.js
+++ app/choices/mutations/createChoice.js
@@ -2,7 +2,7 @@ import { resolver } from "blitz"
 import db from "db"
 import { z } from "zod"
 const CreateChoice = z.object({
-  name: z.string(),
+  text: z.string(),
 })
 export default resolver.pipe(resolver.zod(CreateChoice), resolver.authorize(), async (input) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
