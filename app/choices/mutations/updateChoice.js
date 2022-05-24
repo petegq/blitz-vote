d034a821b5cad506e77ef443da64c36feee9887f
Peter Garside-Quin <petegq@gmail.com>
2022-05-24 13:02:17 +0100

    Update choice to text

--- app/choices/mutations/updateChoice.js
+++ app/choices/mutations/updateChoice.js
@@ -3,7 +3,7 @@ import db from "db"
 import { z } from "zod"
 const UpdateChoice = z.object({
   id: z.number(),
-  name: z.string(),
+  text: z.string(),
 })
 export default resolver.pipe(
   resolver.zod(UpdateChoice),
